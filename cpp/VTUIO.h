a003c168d1a0f0af4bac51bcdb95ad850506ca47
Pengfei Ma <code@pengfeima.cn>
2025-01-02 17:19:35 +0800

    [feat] plot gauss points on the surface

---
+++ cpp/VTUIO.h
@@ -0,0 +1,152 @@
+/// @date 2024-08-08
+/// @file VtuIO.h
+/// @author Ma Pengfei (code@pengfeima.cn)
+/// @version 0.1
+/// @copyright Copyright (c) 2024 Ma Pengfei
+///
+/// @brief
+///
+///
+
+#pragma once
+#include <array>
+#include "pugixml.hpp"
+#include <vector>
+
+namespace {
+[[maybe_unused]] void processStringStream(std::stringstream& sstream, const double& x) { sstream << x << " "; }
+
+template <size_t dim, typename T>
+void processStringStream(std::stringstream& sstream, const std::array<T, dim>& x) {
+    for (size_t i = 0; i < dim; i++) {
+        sstream << x[i] << " ";
+    }
+}
+
+template <typename T>
+struct value_size_of_vector;
+
+template <typename T>
+struct value_size_of_vector<std::vector<T>> {
+    static constexpr std::size_t size = 1;
+};
+
+template <typename T, std::size_t N>
+struct value_size_of_vector<std::vector<std::array<T, N>>> {
+    static constexpr std::size_t size = N;
+};
+
+
+// 将一个vector转换成字符串
+template <typename T, template <typename> typename Vector>
+Vector<std::string> vectors_to_strings(const Vector<T>& first) {
+    Vector<std::string> result;
+    std::stringstream   sstream;
+    for (auto i : first)
+        processStringStream(sstream, i);
+    result.push_back(sstream.str());
+    return result;
+}
+
+template <typename T, template <typename> typename Vector, typename... Args>
+Vector<std::string> vectors_to_strings(const Vector<T>& first, const Args&... rest) {
+    const Vector<std::string>& temp1 = vectors_to_strings(first);
+    const Vector<std::string>& temp2 = vectors_to_strings(rest...);
+    Vector<std::string>        temp;
+    for (auto i : temp1)
+        temp.push_back(i);
+    for (auto i : temp2)
+        temp.push_back(i);
+    return temp;
+}
+} // namespace
+
+namespace IO {
+template <template <typename> typename Vector>
+void write_particles_to_vtu(
+    const int& length, 
+    const std::string& points, 
+    const std::string& filename,
+    const Vector<std::string>& contents, 
+    const Vector<std::string>& tags,
+    const Vector<size_t>& value_sizes) 
+{
+    const size_t num = contents.size();
+    // ASSERT(num == tags.size() && num == value_sizes.size());
+    pugi::xml_document xml_node;
+    auto               vtkfile_node          = xml_node.append_child("VTKFile");
+    vtkfile_node.append_attribute("type")    = "UnstructuredGrid";
+    vtkfile_node.append_attribute("version") = "0.1";
+    {
+        auto u_node = vtkfile_node.append_child("UnstructuredGrid");
+        {
+            auto piece                               = u_node.append_child("Piece");
+            piece.append_attribute("NumberOfPoints") = std::to_string(length).c_str();
+            piece.append_attribute("NumberOfCells")  = "0";
+            {
+                // points coordinates
+                auto node_1     = piece.append_child("Points");
+                auto data_array = node_1.append_child("DataArray");
+
+                data_array.append_attribute("type")               = "Float64";
+                data_array.append_attribute("NumberOfComponents") = "3";
+                data_array.append_attribute("format")             = "ascii";
+                data_array.append_child(pugi::node_pcdata).set_value(points.c_str());
+            }
+            {
+                // cells
+                auto cells_node                        = piece.append_child("Cells");
+                auto dataarray_1                       = cells_node.append_child("DataArray");
+                dataarray_1.append_attribute("type")   = "UInt32";
+                dataarray_1.append_attribute("Name")   = "connectivity";
+                dataarray_1.append_attribute("format") = "ascii";
+                dataarray_1.append_child(pugi::node_pcdata).set_value("");
+                auto dataarray_2                       = cells_node.append_child("DataArray");
+                dataarray_2.append_attribute("type")   = "UInt32";
+                dataarray_2.append_attribute("Name")   = "offsets";
+                dataarray_2.append_attribute("format") = "ascii";
+                dataarray_2.append_child(pugi::node_pcdata).set_value("");
+                auto dataarray_3                       = cells_node.append_child("DataArray");
+                dataarray_3.append_attribute("type")   = "UInt8";
+                dataarray_3.append_attribute("Name")   = "types";
+                dataarray_3.append_attribute("format") = "ascii";
+                dataarray_3.append_child(pugi::node_pcdata).set_value("");
+            }
+
+            auto point_data = piece.append_child("PointData");
+            // point_data.append_attribute("Vectors") = tags[0].c_str();
+            for (size_t i = 0; i < static_cast<size_t>(num); i++) {
+                // points data
+                // spdlog::info(" {}: {}", tags[i], contents[i]);
+                auto data_array = point_data.append_child("DataArray");
+
+                data_array.append_attribute("type")               = "Float64";
+                data_array.append_attribute("Name")               = tags[i].c_str();
+                data_array.append_attribute("NumberOfComponents") = std::to_string(value_sizes[i]).c_str();
+                data_array.append_attribute("format")             = "ascii";
+                data_array.append_child(pugi::node_pcdata).set_value(contents[i].c_str());
+            }
+        }
+    }
+    xml_node.save_file(filename.c_str());
+}
+
+// 文件名、点坐标、标签、内容
+template <typename T, template <typename> typename Vector, typename... Args>
+void write_particles_to_vtu(
+    const std::string& filename, 
+    const Vector<T>& points_raw, 
+    const Vector<std::string>& tags,
+    const Args&... contents_raw) 
+{
+    // ASSERT_INFO(sizeof...(Args) == tags.size(), "{} == {}", sizeof...(Args), tags.size());
+    if constexpr (sizeof...(Args)>0) {
+        auto points   = vectors_to_strings(points_raw)[0];
+        auto contents = vectors_to_strings(contents_raw...);
+        std::vector<size_t> value_sizes = {value_size_of_vector<Args>::size...};
+        write_particles_to_vtu(points_raw.size(), points, filename, contents, tags, value_sizes);
+    } else {
+        // ASSERT_INFO(sizeof...(Args) > 0, "No data to write");
+    }
+}
+}
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,7 @@
 #include <dolfin.h>
 #include "main.h"
 #include "Poisson2D.h"
+#include "VTUIO.h"
 using namespace dolfin;
 
 class DirichletBoundary : public SubDomain
@@ -27,6 +28,8 @@ class FacetIntegration
 {
 private:
     std::vector<std::size_t> _facets;
+    const size_t num_points = 1;
+    const std::vector<std::vector<double>> some_points_all = {{0.5, 0.5}, {0.0, 0.5}, {0.5, 0.0}};
 
 public:
     // Find all id of facets on the given sub_domain
@@ -133,8 +136,7 @@ public:
 
         // TODO: Gauss Quadrature points on local element
         const size_t num_points = 1;
-        const std::vector<std::vector<double>> some_points_all = {{0.5, 0.5},{0.0, 0.5},{0.5, 0.0}};
-
+        const std::vector<std::vector<double>> some_points_all = {{0.5, 0.5}, {0.0, 0.5}, {0.5, 0.0}};
 
         for (std::size_t f = 0; f < _facets.size(); ++f)
         {
@@ -145,12 +147,12 @@ public:
             dolfin_assert(facet.num_entities(D) > 0);
             const std::size_t cell_index = facet.entities(D)[0];
             const Cell cell(mesh, cell_index);
-            
+
             // Get local index of facet with respect to the cell
             const std::size_t local_facet = cell.index(facet);
             // TODO: Get Gauss Quadrature Rule for local facet.
             const auto some_points = some_points_all[local_facet];
-            printf("local_facet: %ld\n",local_facet);
+            printf("local_facet: %ld\n", local_facet);
 
             auto dofs = dofmap.cell_dofs(cell_index);
             disp->vector()->get_local(coefficients.data(), dofs.size(), dofs.data());
@@ -198,6 +200,101 @@ public:
         }
     }
 
+    auto fun3(const std::shared_ptr<Function> &disp, const std::shared_ptr<Function> &force)
+    {
+        // TODO: check if disp and force are on the same function space
+        auto _function_space = disp->function_space();
+        const Mesh &mesh = *_function_space->mesh();
+        const std::size_t D = mesh.topology().dim();
+        const GenericDofMap &dofmap = *_function_space->dofmap();
+        const FiniteElement &element = *_function_space->element();
+        const ufc::finite_element &ufc_element = *element.ufc_element();
+
+        ufc::cell ufc_cell;
+        auto value_size = disp->value_size();
+        auto space_dimension = element.space_dimension();
+        std::vector<double> basis_values(value_size * space_dimension);
+        std::vector<double> coefficients_disp(element.space_dimension());
+        std::vector<double> coefficients_force(element.space_dimension());
+
+        // to hold the results
+        std::vector<double> values_disp_all(_facets.size() * num_points * value_size);
+        std::vector<double> values_force_all(_facets.size() * num_points * value_size);
+
+        for (std::size_t f = 0; f < _facets.size(); ++f)
+        {
+            // Create facet
+            const Facet facet(mesh, _facets[f]);
+
+            // Get cell to which facet belongs.
+            dolfin_assert(facet.num_entities(D) > 0);
+            const std::size_t cell_index = facet.entities(D)[0];
+            const Cell cell(mesh, cell_index);
+
+            // Get local index of facet with respect to the cell
+            const std::size_t local_facet = cell.index(facet);
+            // TODO: Get Gauss Quadrature Rule for local facet.
+            const auto some_points = some_points_all[local_facet];
+            printf("local_facet: %ld\n", local_facet);
+
+            auto dofs = dofmap.cell_dofs(cell_index);
+            disp->vector()->get_local(coefficients_disp.data(), dofs.size(), dofs.data());
+            force->vector()->get_local(coefficients_force.data(), dofs.size(), dofs.data());
+
+            std::vector<double> vertex_coordinates(6);
+            cell.get_vertex_coordinates(vertex_coordinates);
+            printf("vertex_coordinates: %f %f, %f %f, %f %f\n",
+                   vertex_coordinates[0], vertex_coordinates[1], vertex_coordinates[2],
+                   vertex_coordinates[3], vertex_coordinates[4], vertex_coordinates[5]);
+
+            std::vector<double> some_points_local(some_points.size());
+            linearInterpolation<2>(some_points_local.data(), some_points.data(), vertex_coordinates.data(), num_points);
+            std::vector<double> ref_vertex_basis_values(12 * num_points);
+            ufc_element.evaluate_reference_basis(ref_vertex_basis_values.data(), num_points, some_points.data());
+
+            std::vector<double> values_disp(value_size * num_points);
+            std::vector<double> values_force(value_size * num_points);
+
+            for (size_t k = 0; k < num_points; k++)
+            {
+                for (std::size_t i = 0; i < space_dimension; ++i)
+                {
+                    for (std::size_t j = 0; j < value_size; ++j)
+                    {
+                        values_disp[2 * k + j] += coefficients_disp[i] * ref_vertex_basis_values[12 * k + 2 * i + j];
+                        values_force[2 * k + j] += coefficients_force[i] * ref_vertex_basis_values[12 * k + 2 * i + j];
+                    }
+                }
+            }
+
+            for (size_t k = 0; k < num_points; k++)
+            {
+                for (std::size_t j = 0; j < value_size; ++j)
+                {
+                    values_disp_all[f * num_points * value_size + value_size * k + j] = values_disp[value_size * k + j];
+                    values_force_all[f * num_points * value_size + value_size * k + j] = values_force[value_size * k + j];
+                }
+            }
+            // for (size_t i = 0; i < values.size(); i++)
+            // {
+            //     printf("%f ", values[i]);
+            // }
+            // printf("\n");
+
+            for (size_t i = 0; i < 2 * num_points; i++)
+            {
+                printf("%f ", some_points_local[i]);
+            }
+            printf("\n");
+            // for (size_t i = 0; i < 2 * num_points; i++)
+            // {
+            //     printf("%f ", some_points[i]);
+            // }
+            // printf("\n");
+        }
+        return std::make_pair(values_disp_all, values_force_all);
+    }
+
     std::pair<std::vector<double>, std::vector<std::size_t>> basis_values_gauss(
         const Point &point,
         const Cell &cell,
@@ -231,6 +328,27 @@ public:
         return std::make_pair(basis_values, cell_dofmap_vector);
     }
 
+
+
+    void fun4(const std::shared_ptr<Function> &disp, const std::shared_ptr<Function> &force)
+    {
+        auto ret = fun3(disp, force);
+            std::vector<std::array<double, 3>> points0;
+            std::vector<std::array<double, 3>> points;
+            std::vector<std::array<double, 3>> velocities;
+
+        size_t num_points = ret.first.size()/disp->value_size();
+            for (size_t i = 0; i < num_points; i++) {
+                points0.push_back({ret.first[2*i], ret.first[2*i+1], 0.0});
+                points.push_back({ret.first[2*i], ret.first[2*i+1], 0.0});
+                velocities.push_back({ret.first[2*i], ret.first[2*i+1], 0.0});
+            }
+            std::vector<std::array<double, 3>> data1 = points;
+            std::vector<std::array<double, 3>> data2 = velocities;
+
+            std::vector<std::string> tags{"position"};
+            IO::write_particles_to_vtu("a.vtu", points, tags, data2);
+    }
     ~FacetIntegration()
     {
     }
@@ -261,7 +379,13 @@ int main()
     File file_bdry("bdry.pvd");
     file_bdry << *bdry;
 
-    facet_integration.fun2(force);
+    facet_integration.fun4(force,force);
+
+    std::vector<double> some_points = {0.5, 0.5, 0.1, 0.2};
+    auto result = vectors_to_strings(some_points);
+    std::cout << result[0] << std::endl;
+
+
 
     return 0;
 }
