239c6827f2caf91924b633c5fa8e3a0ef7de3815
Pengfei Ma <code@pengfeima.cn>
2025-01-05 15:33:21 +0800

    考虑到使用 c++17 语法，建立一个相对隔离的环境

---
+++ src/gauss_quadrature/a.h
@@ -0,0 +1,12 @@
+//// @date 2025-01-05
+/// @file a.h
+/// @author Ma Pengfei (code@pengfeima.cn)
+/// @version 0.1
+/// @copyright Copyright (c) 2025 Ma Pengfei
+/// 
+/// @brief 
+/// 
+///
+
+
+int a();
