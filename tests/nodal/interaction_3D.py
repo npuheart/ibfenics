d5ad98d4fd7b3f4b4d9e1acfcb5900c11778c7ec
Ma Pengfei <mapengfei@mail.nwpu.edu.cn>
2025-05-06 17:41:04 +0800

    nodal 3D

--- tests/nodal/interaction_3D.py
+++ tests/nodal/interaction_3D.py
@@ -1,59 +1,56 @@
+# Copyright (C) 2024 Pengfei Ma
+#
+# This file is part of ibfenics1 (https://github.com/npuheart/ibfenics1)
+#
+# SPDX-License-Identifier:    LGPL-3.0-or-later
+# email : mapengfei@mail.nwpu.edu.cn
+
 from ibfenics1.cpp import IBMesh3D, IBInterpolation3D
 from fenics import *
-from mshr import Sphere, generate_mesh
 
-
-
-
-
-n_mesh = 32
-points = [Point(0, 0, 0), Point(1, 1, 1)]
-seperations = [n_mesh, n_mesh, n_mesh]
+Ne = 32
+Nl = 10
 order_velocity = 2
+order_pressure = 1
 order_displacement = 2
 
-
-
-ib_mesh = IBMesh3D(points, seperations, order_velocity)
+ib_mesh = IBMesh3D([Point(0, 0, 0), Point(1, 1, 1)], [Ne, Ne, Ne], order_velocity)
 fluid_mesh = ib_mesh.mesh()
-solid_mesh = generate_mesh(Sphere(Point(0.5, 0.5,0.5), 0.2), 30)
-
+solid_mesh = BoxMesh(Point(0.4, 0.4, 0.4), Point(0.6, 0.6, 0.6), Nl, Nl, Nl)
 
 Vf = VectorFunctionSpace(fluid_mesh, "P", order_velocity)
 Vs = VectorFunctionSpace(solid_mesh, "P", order_displacement)
 
-
-
+# 初始化
 uf = interpolate(Expression(("x[0]", "x[1]", "x[2]"), degree=2), Vf)
 ib_mesh.build_map(uf._cpp_object)
-
 inter = IBInterpolation3D(ib_mesh, solid_mesh)
 us = interpolate(Expression(("x[0]", "x[1]", "x[2]"), degree=2), Vs)
 inter.evaluate_current_points(us._cpp_object)
 
 
-# 插值一个点
+# Interpolate a point (观察到二阶精度)
 f = interpolate(Expression(("x[0]*x[0]", "x[1]*x[1]", "x[2]*x[2]"), degree=2), Vf)
-a = ib_mesh.evaluate(Point(0.51,0.52,0.53), uf._cpp_object)
+a = ib_mesh.evaluate(Point(0.51,0.52,0.53), f._cpp_object)
 
-# # 插值一个函数
-# If = Function(Vs)
-# inter.fluid_to_solid(f._cpp_object, If._cpp_object)
-# File("us.pvd") << If
-# File("uf.pvd") << f
-
-
-# 分布一个函数
+# Interpolate a function
+If = Function(Vs)
+inter.fluid_to_solid(f._cpp_object, If._cpp_object)
+File("us.pvd") << If
+File("uf.pvd") << f
 
 
+# spread a function
 Es = Function(Vf)
 us = interpolate(Expression(("x[0]", "x[1]", "x[2]"), degree=2), Vs)
 
+# 1. 先将函数与测试函数做内积
 dus = TestFunction(Vs)
 Ls = inner(us, dus)*dx
 bs = assemble(Ls)
 us.vector()[:] = bs[:]
 
+# 2. 然后将内积结果传递给流体
 inter.solid_to_fluid(Es._cpp_object, us._cpp_object)
 File("uf_.pvd") << Es
 File("us_.pvd") << us
