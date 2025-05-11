7a35e504423d5b7dda34982db71a5287da119e7a
Ma Pengfei <mapengfei@mail.nwpu.edu.cn>
2025-05-11 16:37:42 +0800

    .

--- tests/nodal/ideal_valve/ideal_valve_2D.py
+++ tests/nodal/ideal_valve/ideal_valve_2D.py
@@ -14,14 +14,25 @@ from ibfenics1.io import (
     write_excel,
     write_excel_sheets,
 )
+beta = 1e8
+# C0=2e5
+C0=0
+C1=1e6
+# kappa_s=4e5
+kappa_s=0
+
+ds = Measure('ds', domain=solid_mesh, subdomain_data=boundaries)
 
 nssolver = "projection"
+material = "f1" # "n1", "n2", "f0", "f1", "f2", "f3"
 
 swanlab.login(api_key="VBxEp1UBe2606KHDM9264", save=True)
 swanlab.init(
     project=os.path.splitext(os.path.basename(__file__))[0],
-    experiment_name=f"dt_{dt}_Ne_{Ne}_{nssolver}",
+    experiment_name=f"dt_{dt}_beta_{beta}_C0_{C0}_C1_{C1}_ks_{kappa_s}_Ne_{Ne}_Nl_{Nl}_{material}",
     description="二维理想瓣膜",
+    config={'dt': dt, 'beta': beta, 'C0': C0, 'C1':C1, 'ks':kappa_s, 'Ne': Ne, 'Nl': Nl, 'material': material},
+    
 )
 
 u0 = Function(Vf, name="velocity")
@@ -43,16 +54,69 @@ swanlab.config['file_solid'] = file_solid_name
 
 # Solid Mechanics
 # from ufl import ln
-mu_s = 5.6e5
-nv_s = 0.4
-lambda_s = 2*mu_s*(1-nv_s)/3.0/(1-2*nv_s)
+
 U0 = Function(Vs, name="velocity")
 X0 = interpolate(Expression(("x[0]", "x[1]"), degree=2), Vs)
+X_start = interpolate(Expression(("x[0]", "x[1]"), degree=2), Vs)
 F0 = Function(Vs, name="force")
 
-dVs = TestFunction(Vs)
-FF = grad(X0)
-L_hat = - inner(mu_s*(FF-inv(FF).T) + lambda_s*ln(det(FF))*inv(FF).T, grad(dVs))*dx
+def MaterialModel_1(X0):
+    # 计算形变梯度
+    FF = grad(X0)
+    # 本构参数
+    mu_s = 5.6e5
+    nv_s = 0.4
+    lambda_s = 2*mu_s*(1-nv_s)/3.0/(1-2*nv_s)
+    dVs = TestFunction(Vs)
+    L_hat = - inner(mu_s*(FF-inv(FF).T) + lambda_s*ln(det(FF))*inv(FF).T, grad(dVs))*dx
+    return L_hat
+
+def MaterialModel_2(X0):
+    # 计算形变梯度
+    FF = grad(X0)
+    # 本构参数
+    mu_s = 5.6e5
+    dVs = TestFunction(Vs)
+    L_hat = - inner(mu_s*(FF-inv(FF).T), grad(dVs))*dx
+    return L_hat
+
+f1 = Constant((1.0, 0.0))
+f2 = Constant((0.7071067811865475,0.7071067811865475))
+f3 = Constant((0.0, 1.0))
+def MaterialModel_3(X0, fiber, C0, C1, kappa_s):
+    # 计算形变梯度
+    FF = variable(grad(X0))
+    J = det(FF)
+    FF_bar = J**(-1/2)*FF
+    CC_bar = FF_bar.T*FF_bar
+    CC = FF.T*FF
+    I1_bar = tr(CC_bar)
+    m = fiber
+    I4 = dot(m, CC*m)
+    I4_bar = dot(m, CC_bar*m)
+    # 本构参数
+    Psi = 0.5*C0*(I1_bar-3) + C1*(exp(I4_bar-1) - I4_bar) + 0.5*kappa_s*(0.5*(J*J-1)-ln(J))
+    dPsi = diff(Psi, FF)    
+    dVs = TestFunction(Vs)
+    L_hat = - inner(dPsi, grad(dVs))*dx
+    return L_hat
+
+
+if material == "n1":
+    L_hat = MaterialModel_1(X0)
+elif material == "n2":
+    L_hat = MaterialModel_2(X0)
+elif material == "f0":
+    L_hat = MaterialModel_3(X0, f1, C0=C0, C1=0.0, kappa_s=kappa_s)
+elif material == "f1":
+    L_hat = MaterialModel_3(X0, f1, C0=C0, C1=C1, kappa_s=kappa_s)
+elif material == "f2":
+    L_hat = MaterialModel_3(X0, f2, C0=C0, C1=C1, kappa_s=kappa_s)
+elif material == "f3":
+    L_hat = MaterialModel_3(X0, f3, C0=C0, C1=C1, kappa_s=kappa_s)
+
+L_hat += beta*inner(X_start-X0, TestFunction(Vs))*ds(1)
+
 # L_hat = - inner(mu_s*(FF-inv(FF).T), grad(dVs))*dx
 # 
 t = 0
--- tests/nodal/ideal_ventricle_3D/local_mesh.py
+++ tests/nodal/ideal_ventricle_3D/local_mesh.py
@@ -1,7 +1,7 @@
 from fenics import VectorFunctionSpace, FunctionSpace, Point
 from fenics import DirichletBC, Expression, Constant, interpolate
-from fenics import RectangleMesh, Mesh, MeshEditor, CellType, near, MeshFunction, SubDomain
-from ibfenics1.cpp import IBMesh, IBInterpolation
+from fenics import BoxMesh, Mesh, MeshEditor, CellType
+from ibfenics1.cpp import IBMesh3D, IBInterpolation3D
 import numpy as np
 
 def connect_mesh(mesh0, mesh1):
@@ -38,11 +38,11 @@ def connect_mesh(mesh0, mesh1):
 # Parameters
 nv = 0.1
 T = 10.0
-dt = 5e-5
+dt = 1e-3
 num_steps = int(T/dt)
 rho = 1.0
 Nl = 4
-Ne = 32
+Ne = 20
 dt_minimum = 1e-5
 
 # Mesh
@@ -50,21 +50,10 @@ order_velocity = 2
 order_pressure = 1
 order_displacement = 2
 
-ib_mesh = IBMesh([Point(0, 0), Point(8, 1.61)], [4*Ne, Ne], order_velocity)
+
+ib_mesh = IBMesh3D([Point(0, 0, 0), Point(1, 1, 1)], [Ne, Ne, Ne], order_velocity)
 fluid_mesh = ib_mesh.mesh()
-solid_mesh_0 = RectangleMesh(Point(2.0, 0.0), Point(2.0212, 0.7), Nl, Nl*20)
-# solid_mesh = RectangleMesh.create([Point(2.0, 0.0), Point(2.0212, 0.7)], [Nl, Nl*20], CellType.Type.quadrilateral)
-solid_mesh_1 = RectangleMesh(Point(2.0, 0.91), Point(2.0212, 1.61), Nl, Nl*20)
-solid_mesh = connect_mesh(solid_mesh_0, solid_mesh_1)
-
-boundaries = MeshFunction("size_t", solid_mesh, solid_mesh.topology().dim()-1)
-boundaries.set_all(0)
-class BottomBoundary(SubDomain):
-    def inside(self, x, on_boundary):
-        return near(x[1], 0.0) or near(x[1], 1.61) 
-
-bottom = BottomBoundary()
-bottom.mark(boundaries, 1)  
+solid_mesh = BoxMesh(Point(0.4, 0.4, 0.4), Point(0.6, 0.6, 0.6), Nl, Nl, Nl)
 
 # Define function spaces
 Qf = FunctionSpace(fluid_mesh, "P", order_pressure)
@@ -72,30 +61,24 @@ Vf = VectorFunctionSpace(fluid_mesh, "P", order_velocity)
 Vs = VectorFunctionSpace(solid_mesh, "P", order_displacement)
 
 # 初始化
-uf = interpolate(Expression(("x[0]", "x[1]"), degree=2), Vf)
+uf = interpolate(Expression(("x[0]", "x[1]", "x[2]"), degree=2), Vf)
 ib_mesh.build_map(uf._cpp_object)
-inter = IBInterpolation(ib_mesh, solid_mesh)
-us = interpolate(Expression(("x[0]", "x[1]"), degree=2), Vs)
+inter = IBInterpolation3D(ib_mesh, solid_mesh)
+us = interpolate(Expression(("x[0]", "x[1]", "x[2]"), degree=2), Vs)
 inter.evaluate_current_points(us._cpp_object)
 
 
 
 
 
-
-
-
-
-
-flow_velocity = Expression(("5 * (sin(2 * M_PI * t) + 1.1) * x[1] * (1.61 - x[1]);", "0.0"), degree=1, t=0.0)
-
-
 def calculate_fluid_boundary_conditions(Vf, Qf):
-    bcu_inflow = DirichletBC(Vf, flow_velocity, "near(x[0],0.0)")
-    bcu_wall = DirichletBC(Vf, Expression(("0.0", "0.0"), degree=1), "near(x[1],0.0) || near(x[1],1.61)")
-    bcp_outlet = DirichletBC(Qf, Expression("0.0", degree=1), "near(x[0],8.0)")
+    flow_velocity = Expression(("1.0", "0.0", "0.0"), degree=1)
+    bcu_inflow = DirichletBC(Vf, flow_velocity, "near(x[1],1.0)")
+    bcu_wall = DirichletBC(Vf, Expression(("0.0", "0.0", "0.0"), degree=1), 
+                           "near(x[0],0.0) || near(x[0],1) || near(x[1],0.0) || near(x[2],0.0) || near(x[2],1.0)")
+    # bcp_outlet = DirichletBC(Qf, Expression("0.0", degree=1), "near(x[0],8.0)")
     bcu = [bcu_inflow, bcu_wall]
-    bcp = [bcp_outlet]
+    bcp = []
     return bcu, bcp
 
 
@@ -134,18 +117,16 @@ __all__ = [
     "Vs",
     "Vf",
     "Qf",
-    "flow_velocity",
+    # "flow_velocity",
     "inter",
     "fluid_mesh",
     "solid_mesh",
-    "boundaries",
     "calculate_fluid_boundary_conditions",
     "nv",
     "T",
     "num_steps",
     "dt_minimum",
     "Ne",
-    "Nl",
     "dt",
     "rho",
     "extract_over_mid_x",
